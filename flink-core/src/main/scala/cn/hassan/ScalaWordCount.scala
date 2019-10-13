9acdd4e7efb223f04f90d34eb65dbcc21b9c979f
hassan <hassanbox@qq.com>
2019-10-13 18:59:36 +0800

    flink

---
+++ flink-core/src/main/scala/cn/hassan/ScalaWordCount.scala
@@ -0,0 +1,17 @@
+package cn.hassan
+
+import org.apache.flink.api.scala.ExecutionEnvironment
+
+import org.apache.flink.api.scala._
+
+object ScalaWordCount {
+
+  def main(args: Array[String]): Unit = {
+    // 1 env 2 source 3 transform 4 sink
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    val path = Thread.currentThread().getContextClassLoader.getResource("data.txt").getPath
+    val dataSet = env.readTextFile(path)
+    val result = dataSet.flatMap(_.split(" ")).map((_,1)).groupBy(0).sum(1)
+    result.print()
+  }
+}
