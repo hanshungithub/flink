e78d341c925b2f538c1a6cd569fb3df76c5b4b7e
hassan <hassanbox@qq.com>
2019-10-13 19:21:51 +0800

    flink

--- flink-core/src/main/scala/cn/hassan/DStreamWordCount.scala
+++ flink-core/src/main/scala/cn/hassan/DStreamWordCount.scala
@@ -7,8 +7,9 @@ import org.apache.flink.api.scala._
 object DStreamWordCount {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
-    val dStream = env.socketTextStream("hadoop101",7777)
+    val dStream = env.socketTextStream("127.0.0.1",7777)
     val result = dStream.flatMap(_.split(" ")).filter(_.nonEmpty).map((_,1)).keyBy(0).sum(1)
     result.print()
+    env.execute()
   }
 }
